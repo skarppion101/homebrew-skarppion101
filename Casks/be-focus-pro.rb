a6588ca73ddd07d49ec4453dbf53c6f32c6508f1
Alex Semenov <skarppi@mail.ru>
2022-09-21 09:29:37 +0300

    update

---
+++ Casks/be-focus-pro.rb
@@ -0,0 +1,11 @@
+cask "be-focus-pro" do
+  version "2.2"
+  sha256 "99ce23416ad2c8983a28785f3a01d547d2f000ff7c28d664ababec34ad4dcdf2"
+
+  url "https://vk.com/s/v1/doc/U_eEuaKKpUc4DVj7JQ2amYCAez7s-8d-mzHfVdZRzkWvzPVCLxc"
+  name "Be Focused Pro"
+  desc "Be Focused Pro"
+  homepage "https://appstorrent.ru/374-be-focused.html"
+
+  app "Be Focused Pro.app"
+end
