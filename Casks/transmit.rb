d4a609fd8f41ce4b7ab8aa0afe717a23d33974d6
Alex Semenov <skarppi@mail.ru>
2022-09-21 09:13:53 +0300

    update

--- Casks/transmit.rb
+++ Casks/transmit.rb
@@ -1,7 +1,8 @@
 cask "transmit" do
   version "5.8.10"
+  sha256 "698686bdad59bd573bedd79636e3a3379324514e997e05a010855dd39918fdf6"
 
-  url "https://vk.com/s/v1/doc/fQPCP2HBIClX1o_PzfOvwfc1uAtDsDnnv9lYSTBV1kiZjASwHvY"
+  url "https://vk.com/s/v1/doc/0MY68W2pLP1RGNl9wBg_wyRF8De-UgLYzd-n52okSAEoiTJWmQk"
   name "Transmit"
   desc "Transmit FTP client"
   homepage "https://appstorrent.ru/162-transmit.html"
