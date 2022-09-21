c9b9a489838a075280c9515c90569eca6ac4cd4b
Alex Semenov <skarppi@mail.ru>
2022-09-21 09:11:21 +0300

    update

--- Casks/transmit.rb
+++ Casks/transmit.rb
@@ -1,6 +1,5 @@
 cask "transmit" do
   version "5.8.10"
-  sha256 "698686bdad59bd573bedd79636e3a3379324514e997e05a010855dd39918fdf6"
 
   url "https://vk.com/s/v1/doc/fQPCP2HBIClX1o_PzfOvwfc1uAtDsDnnv9lYSTBV1kiZjASwHvY"
   name "Transmit"
